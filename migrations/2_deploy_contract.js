1c2deb98f9d13a254607c1b150b979a847429e83
zxj <zxj@33.cn>
2021-11-19 18:23:16 +0800

    init

---
+++ migrations/2_deploy_contract.js
@@ -0,0 +1,5 @@
+const DigitalMuseum = artifacts.require("DigitalMuseum");
+
+module.exports = function (deployer) {
+  deployer.deploy(DigitalMuseum);
+};
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    {{balance}}
   </div>
 </template>
+
+<script setup lang="ts">
+import {reactive, ref} from 'vue'
+import web3 from 'web3'
+
+let currentProvider = reactive({})
+currentProvider = web3.givenProvider? web3.givenProvider:'http://127.0.0.1:7545'
+
+
+const balance = ref(0)
+
+
+</script>
+
+<style scoped>
+
+</style>
