ab08a72f5bac6e62d1f00ffb46cc5596a04b511f
zxj <zxj@33.cn>
2022-02-14 17:43:19 +0800

    feat: auction contract

--- src/Web3/contracts/Market.sol
+++ src/Web3/contracts/Market.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
@@ -18,15 +19,29 @@ contract Market is ReentrancyGuard {
         owner = payable(msg.sender);
     }
 
+    /* 
+        BuyNow: The item can be sold in a fixed price
+        OnAuction: The item can be sold in auction
+        Sold: The item has been sold
+        New: Unsold
+     */
+
+    enum Status {
+        BuyNow,
+        OnAuction,
+        Sold,
+        New
+    }
+
     struct MarketItem {
         uint256 itemId;
-        address nftContractAdd;
         uint256 tokenId;
         uint256 price;
         address payable seller;
         address payable owner;
-        bool sold;
+        Status status;
     }
+
     /* map the itemId to the Item */
     mapping(uint256 => MarketItem) private idToMktItem;
 
@@ -38,20 +53,18 @@ contract Market is ReentrancyGuard {
     /* Places an nft to the market */
     event MarketItemCreated(
         uint256 indexed itemId,
-        address indexed nftContractAdd,
         uint256 indexed tokenId,
-        uint256 price,
+        address indexed nftContract,
         address payable seller,
         address payable owner,
-        bool sold
+        Status status
     );
 
-    function createMarketItem(
-        address contractAdd,
-        uint256 tokenId,
-        uint256 price
-    ) public payable nonReentrant {
-        require(price > 0, "Price should be at least 1wei");
+    function createMarketItem(address nftContract, uint256 tokenId)
+        public
+        payable
+        nonReentrant
+    {
         require(
             msg.value == listingPrice,
             "Price must be equal to listing price"
@@ -61,25 +74,55 @@ contract Market is ReentrancyGuard {
 
         idToMktItem[itemId] = MarketItem(
             itemId,
-            contractAdd,
             tokenId,
-            price,
+            0,
             payable(msg.sender),
             payable(address(0)),
-            false
+            Status.New
         );
 
-        IERC721(contractAdd).transferFrom(msg.sender, address(this), tokenId);
-
+        /* Transfers the nft from the creator to the contract */
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
         emit MarketItemCreated(
             itemId,
-            contractAdd,
             tokenId,
-            price,
+            nftContract,
             payable(msg.sender),
             payable(address(0)),
-            false
+            Status.New
         );
+    }
+
+    function setFixedPrice(uint256 _itemId, uint256 _price) public {
+        require(idToMktItem[_itemId].status == Status.New);
+        idToMktItem[_itemId].status = Status.BuyNow;
+        idToMktItem[_itemId].price = _price;
+    }
+
+    // Creates Auction
+
+    function createAuction(
+        uint256 itemId,
+        uint256 duration,
+        uint256 reservePrice,
+        address contractAdd
+    ) public payable nonReentrant auctionNonExistant(itemId) {
+        require(idToMktItem[itemId].status == Status.New);
+        // Initialize the auction details, including null values.
+        auctions[itemId] = Auction({
+            duration: duration,
+            reservePrice: reservePrice,
+            curator: owner,
+            amount: 0,
+            firstBidTime: 0,
+            bidder: payable(address(0))
+        });
+
+        // Transfer the NFT into this auction contract, from whoever owns it.
+        uint256 tokenId = idToMktItem[itemId].tokenId;
+        IERC721(contractAdd).transferFrom(msg.sender, address(this), tokenId);
+        // Emit an event describing the new auction.
+        emit AuctionCreated(tokenId, duration, reservePrice);
     }
 
     /* Creates the sale of a market item */
@@ -92,14 +135,18 @@ contract Market is ReentrancyGuard {
         uint256 price = idToMktItem[itemId].price;
         uint256 tokenId = idToMktItem[itemId].tokenId;
         require(
-            price == msg.value/(1 ether),
+            idToMktItem[itemId].status == Status.BuyNow,
+            "Please submit the asking price in order to complete the purchase"
+        );
+        require(
+            price == msg.value / (1 ether),
             "Please submit the asking price in order to complete the purchase"
         );
 
         idToMktItem[itemId].seller.transfer(msg.value);
         IERC721(contractAdd).transferFrom(address(this), msg.sender, tokenId);
         idToMktItem[itemId].owner = payable(msg.sender);
-        idToMktItem[itemId].sold = true;
+        idToMktItem[itemId].status = Status.Sold;
         _itemsSold.increment();
         payable(owner).transfer(listingPrice);
     }
@@ -111,7 +158,7 @@ contract Market is ReentrancyGuard {
         uint256 currenIndex = 0;
 
         MarketItem[] memory items = new MarketItem[](unsoldItemsCount);
-        for (uint i = 1; i <= totalItems; i++) {
+        for (uint256 i = 1; i <= totalItems; i++) {
             if (idToMktItem[i].owner == address(0)) {
                 MarketItem storage currentItem = idToMktItem[i];
                 items[currenIndex] = currentItem;
@@ -127,14 +174,14 @@ contract Market is ReentrancyGuard {
         uint256 currenIndex = 0;
         uint256 myItemsCount = 0;
 
-        for(uint i = 1; i <= totalItems; i++) {
+        for (uint256 i = 1; i <= totalItems; i++) {
             if (idToMktItem[i].owner == msg.sender) {
-                myItemsCount +=1;
+                myItemsCount += 1;
             }
         }
 
         MarketItem[] memory items = new MarketItem[](myItemsCount);
-        for (uint i = 1; i <= totalItems; i++) {
+        for (uint256 i = 1; i <= totalItems; i++) {
             if (idToMktItem[i].owner == msg.sender) {
                 MarketItem storage currentItem = idToMktItem[i];
                 items[currenIndex] = currentItem;
@@ -150,14 +197,14 @@ contract Market is ReentrancyGuard {
         uint256 currenIndex = 0;
         uint256 createdItemsCount = 0;
 
-        for(uint i = 1; i <= totalItems; i++) {
+        for (uint256 i = 1; i <= totalItems; i++) {
             if (idToMktItem[i].seller == msg.sender) {
-                createdItemsCount +=1;
+                createdItemsCount += 1;
             }
         }
 
         MarketItem[] memory items = new MarketItem[](createdItemsCount);
-        for (uint i = 1; i <= totalItems; i++) {
+        for (uint256 i = 1; i <= totalItems; i++) {
             if (idToMktItem[i].seller == msg.sender) {
                 MarketItem storage currentItem = idToMktItem[i];
                 items[currenIndex] = currentItem;
@@ -167,4 +214,217 @@ contract Market is ReentrancyGuard {
         return items;
     }
 
+    /* Auction */
+
+    // Use OpenZeppelin's SafeMath library to prevent overflows.
+    using SafeMath for uint256;
+
+    // ============ Constants ============
+
+    // The minimum amount of time left in an auction after a new bid is created; 15 min.
+    uint16 public constant TIME_BUFFER = 60;
+    // The ETH needed above the current bid for a new bid to be valid; 0.001 ETH.
+    uint8 public constant MIN_BID_INCREMENT_PERCENT = 10;
+    // Interface constant for ERC721, to check values in constructor.
+    bytes4 private constant ERC721_INTERFACE_ID = 0x80ac58cd;
+
+    // A mapping of all of the auctions currently running.
+    mapping(uint256 => Auction) public auctions;
+
+    // ============ Structs ============
+
+    struct Auction {
+        // The value of the current highest bid.
+        uint256 amount;
+        // The amount of time that the auction should run for,
+        // after the first bid was made.
+        uint256 duration;
+        // The time of the first bid.
+        uint256 firstBidTime;
+        // The minimum price of the first bid.
+        uint256 reservePrice;
+        // The address of the current highest bid.
+        address payable bidder;
+        // The address of the auction's curator. The curator
+        // can cancel the auction if it hasn't had a bid yet.
+        address curator;
+    }
+
+    // ============ Events ============
+
+    // All of the details of a new auction,
+    // with an index created for the tokenId.
+    event AuctionCreated(
+        uint256 indexed tokenId,
+        uint256 duration,
+        uint256 reservePrice
+    );
+
+    // All of the details of a new bid,
+    // with an index created for the tokenId.
+    event AuctionBid(uint256 indexed tokenId, address sender, uint256 value);
+
+    // All of the details of an auction's cancelation,
+    // with an index created for the tokenId.
+    event AuctionCanceled(uint256 indexed tokenId);
+
+    // All of the details of an auction's close,
+    // with an index created for the tokenId.
+    event AuctionEnded(uint256 indexed tokenId, address winner, uint256 amount);
+
+    // ============ Modifiers ============
+
+    // Reverts if the auction does not exist.
+    modifier auctionExists(uint256 itemId) {
+        // The auction exists if the curator is not null.
+        require(!auctionCuratorIsNull(itemId), "Auction doesn't exist");
+        _;
+    }
+
+    // Reverts if the auction exists.
+    modifier auctionNonExistant(uint256 itemId) {
+        // The auction does not exist if the curator is null.
+        require(auctionCuratorIsNull(itemId), "Auction already exists");
+        _;
+    }
+
+    // Reverts if the auction is expired.
+    modifier auctionNotExpired(uint256 itemId) {
+        require(
+            // Auction is not expired if there's never been a bid, or if the
+            // current time is less than the time at which the auction ends.
+            auctions[itemId].firstBidTime == 0 ||
+                block.timestamp < auctionEnds(itemId),
+            "Auction expired"
+        );
+        _;
+    }
+
+    // Reverts if the auction is not complete.
+    // Auction is complete if there was a bid, and the time has run out.
+    modifier auctionComplete(uint256 itemId) {
+        require(
+            // Auction is complete if there has been a bid, and the current time
+            // is greater than the auction's end time.
+            auctions[itemId].firstBidTime > 0 &&
+                block.timestamp >= auctionEnds(itemId),
+            "Auction hasn't completed"
+        );
+        _;
+    }
+
+    // ============ Create Bid ============
+
+    function createBid(uint256 itemId, uint256 amount)
+        public
+        payable
+        nonReentrant
+        auctionExists(itemId)
+        auctionNotExpired(itemId)
+    {
+        // Validate that the user's expected bid value matches the ETH deposit.
+        require(amount == msg.value / (1 ether), "Amount doesn't equal msg.value");
+        require(amount > 0, "Amount must be greater than 0");
+        // Check if the current bid amount is 0.
+        if (auctions[itemId].amount == 0) {
+            // If so, it is the first bid.
+            auctions[itemId].firstBidTime = block.timestamp;
+            // We only need to check if the bid matches reserve bid for the first bid,
+            // since future checks will need to be higher than any previous bid.
+            require(
+                amount >= auctions[itemId].reservePrice,
+                "Must bid reservePrice or more"
+            );
+        } else {
+            // Check that the new bid is sufficiently higher than the previous bid, by
+            // the percentage defined as MIN_BID_INCREMENT_PERCENT.
+            require(
+                amount >=
+                    auctions[itemId].amount.add(
+                        // Add 10% of the current bid to the current bid.
+                        auctions[itemId]
+                            .amount
+                            .mul(MIN_BID_INCREMENT_PERCENT)
+                            .div(100)
+                    ) /
+                        (1 ether),
+                "Must bid more than last bid by MIN_BID_INCREMENT_PERCENT amount"
+            );
+
+            // Refund the previous bidder.
+            auctions[itemId].bidder.transfer(auctions[itemId].amount);
+        }
+        // Update the current auction.
+        auctions[itemId].amount = amount;
+        auctions[itemId].bidder = payable(msg.sender);
+        // Compare the auction's end time with the current time plus the 15 minute extension,
+        // to see whether we're near the auctions end and should extend the auction.
+        if (auctionEnds(itemId) < block.timestamp.add(TIME_BUFFER)) {
+            // We add onto the duration whenever time increment is required, so
+            // that the auctionEnds at the current time plus the buffer.
+            auctions[itemId].duration += block.timestamp.add(TIME_BUFFER).sub(
+                auctionEnds(itemId)
+            );
+        }
+        // Emit the event that a bid has been made.
+        emit AuctionBid(itemId, msg.sender, amount);
+    }
+
+    // ============ End Auction ============
+    function endAuction(uint256 itemId, address nftContract)
+        public
+        payable
+        nonReentrant
+        auctionComplete(itemId)
+    {
+        require(auctions[itemId].amount != 0, "This item is not on auction");
+        require(msg.sender ==  auctions[itemId].bidder, "Only the bidder can end auction");
+        // Store relevant auction data in memory for the life of this function.
+        address winner = auctions[itemId].bidder;
+        uint256 amount = auctions[itemId].amount;
+        // Remove all auction data for this token from storage.
+        delete auctions[itemId];
+        // We don't use safeTransferFrom, to prevent reverts at this point,
+        // which would break the auction.
+        uint256 tokenId = idToMktItem[itemId].tokenId;
+        IERC721(nftContract).transferFrom(address(this), winner, tokenId);
+        idToMktItem[itemId].owner = payable(winner);
+        idToMktItem[itemId].status = Status.Sold;
+        idToMktItem[itemId].price = amount;
+        _itemsSold.increment();
+        // Emit an event describing the end of the auction.
+        emit AuctionEnded(itemId, winner, amount);
+    }
+
+    // ============ Cancel Auction ============
+
+    function cancelAuction(uint256 _itemId)
+        public
+        nonReentrant
+        auctionExists(_itemId)
+    {
+        // Check that there hasn't already been a bid for this NFT.
+        require(
+            uint256(auctions[_itemId].firstBidTime) == 0,
+            "Auction already started"
+        );
+        // Remove all data about the auction.
+        delete auctions[_itemId];
+        // Emit an event describing that the auction has been canceled.
+        emit AuctionCanceled(_itemId);
+    }
+
+    // Returns true if the auction's curator is set to the null address.
+    function auctionCuratorIsNull(uint256 itemId) private view returns (bool) {
+        // The auction does not exist if the curator is the null address,
+        // since the NFT would not have been transferred in `createAuction`.
+        return auctions[itemId].curator == address(0);
+    }
+
+    // Returns the timestamp at which an auction will finish.
+    function auctionEnds(uint256 itemId) private view returns (uint256) {
+        // Derived by adding the auction's duration to the time of the first bid.
+        // NOTE: duration can be extended conditionally after each new bid is added.
+        return auctions[itemId].firstBidTime.add(auctions[itemId].duration);
+    }
 }
